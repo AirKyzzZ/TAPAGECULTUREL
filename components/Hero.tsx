1ac268503bb6d4051db6221942e86f8a877731f9
AirKyzzZ <airkyzzz.jiji@gmail.com>
2025-08-28 12:54:57 -0400

    fix: scroll icon center fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -45,7 +45,7 @@ export default function Hero({ headline, sub, backgroundImage = '/assets/hero-bg
       </div>
       
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-tapage-periglacial rounded-full flex justify-center">
           <div className="w-1 h-3 bg-tapage-periglacial rounded-full mt-2 animate-pulse"></div>
         </div>
